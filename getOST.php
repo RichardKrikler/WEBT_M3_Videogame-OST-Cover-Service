36cee7c0b85dc8694b9b0d51a94fac866e6b6d5f
Jakob Mucherl <jakob.mucherl@htl.rennweg.at>
2021-12-07 08:57:48 +0100

    AD-US1

--- getOST.php
+++ getOST.php
@@ -3,9 +3,10 @@ require_once 'Seeder.php';
 header('content-type:application/json');
 
 $osts = (new Seeder())->getOsts();
+$saveGET = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['id']);
 if (isset($_GET['id'])) {
     foreach ($osts as $ost) {
-        if ($ost->getId() == $_GET['id']) {
+        if ($ost->getId() == $saveGET) {
             echo json_encode($ost);
             return;
         }
